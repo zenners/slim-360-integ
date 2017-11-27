e59eb54dc97fa3215cc522c8b360046c5404c908
zenners <cu.michaels@gmail.com>
2017-11-27 10:48:18 +0800

    first commit

---
+++ src/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Routes
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post('/application', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+    $data = trim(file_get_contents("php://input"));
+
+    $ch = curl_init('https://api360.fundko.com/Service1.svc/process360Test');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data))
+    );
+
+    $result = curl_exec($ch);
+    return $data;
+});
