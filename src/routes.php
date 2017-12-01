28fa624408b409d2a47e001364f6a3deff2998a0
Siena Santos <aneis_siena18@yahoo.com>
2017-12-01 14:02:09 +0800

    log

--- src/routes.php
+++ src/routes.php
@@ -17,8 +17,9 @@ $app->post('/application', function (Request $request, Response $response, array
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
     $data = trim(file_get_contents("php://input"));
-    echo '<script>console.log("data input from cst "'.$data.')</script>';
-    $this->logger->debug($data);
+    // echo '<script>console.log("data input from cst "'.$data.')</script>';
+    // $this->logger->debug($data);
+    print $data;
 
 
     $ch = curl_init('https://api360.fundko.com/Service1.svc/process360Test');
